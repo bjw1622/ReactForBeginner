4baf4b7c0e1fd75fe77959ed898e31f6060cc0e7
Jay <bjw1622@gmail.com>
2022-06-28 14:49:14 +0900

    Add 자료형

---
+++ JS_Review/자료형.js
@@ -0,0 +1,7 @@
+// Data Types은 Primitive Data Type, Non-Primitive Data Type이 존재
+
+// Primitive Data Type은(원시형) Number, String, Boolean, Undefined, Null
+// 한번에 하나의 값만
+
+// Non-Primitive Data Type은(비원시형) Object, Array, Function이 존재 
+// 한번에 여러개의 값
